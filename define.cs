5d6cd35c0bd34758707c5d5fa9b6fb0144b0de64
cnzoupeng <zou.peng.cn@gmail.com>
2013-09-12 23:10:03 +0800

    update history form cpp

--- define.cs
+++ define.cs
@@ -5,6 +5,8 @@
         public const uint RED_ITEM_COUNT = 6;
         public const string HISTORY_FILE = "history.txt";
         public const string WEB_DATA_URL = "http://42.121.193.75";
+
+        
     }
 
     public class SelectRec
@@ -14,5 +16,46 @@
         public int col_begin = -1;
         public int col_end = -1;
         public int col_map = 0;
+    }
+
+    //
+    public class LtNet
+    {
+        public const uint LT_MAGIC = 0x56ADF65B;
+    }
+
+    enum JobId
+    {
+	    JOB_MIN = 900001,
+	    JOB_HISTORY = JOB_MIN,
+	    JOB_VERSION,
+	    JOB_FILE,
+	    JOB_MAX = JOB_FILE
+    };
+
+    public struct CommHead
+    {
+        public int key;
+        public int magic;
+        public int job;
+        public int len;
+    }
+
+    public struct ReqHis
+    {
+        public int start;
+        public int stop;
+    }
+
+    public struct ReqHisData
+    {
+        public CommHead head;
+        public ReqHis req;
+    }
+
+    public struct ResData
+    {
+        public CommHead head;
+        public int result;
     }
 }
--- ltData.cs
+++ ltData.cs
@@ -6,9 +6,12 @@ using System.Text;
 using System.Threading;
 using System.ComponentModel;
 using System;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 namespace NewLt
 {
+
     public class LtData
     {
         public ArrayList ltSet;
@@ -81,6 +84,32 @@ namespace NewLt
             return ltSet.Count;
         }
 
+        public static byte[] StructToBytes(object structObj)
+        {
+            int size = Marshal.SizeOf(structObj);  
+            byte[] bytes = new byte[size]; 
+            IntPtr structPtr = Marshal.AllocHGlobal(size);      
+            Marshal.StructureToPtr(structObj, structPtr, false);
+            Marshal.Copy(structPtr, bytes, 0, size);   
+            Marshal.FreeHGlobal(structPtr);   
+            return bytes;
+        }
+
+        public object BytesToStruct(byte[] bytes, Type type)
+        {
+            int size = Marshal.SizeOf(type);
+            if (size > bytes.Length)
+            {
+                return null;
+            }
+
+            IntPtr structPtr = Marshal.AllocHGlobal(size);
+            Marshal.Copy(bytes, 0, structPtr, size);
+            object obj = Marshal.PtrToStructure(structPtr, type);
+            Marshal.FreeHGlobal(structPtr);
+            return obj;
+        }
+
         public int load_history()
         {
             ltSet.Clear();
@@ -94,10 +123,153 @@ namespace NewLt
                 load_from_file(DEFINE.HISTORY_FILE);
             }
 
-            var th2 = new Thread(LtData.update_history);
+            var th2 = new Thread(LtData.get_new);
             th2.Start(this);
 
             return 0;
+        }
+
+        public static void get_new(object ltd)
+        {
+            LtData ltData = (LtData)ltd;
+
+            int tryTimes = 3;
+            int start = 2003001;
+            int stop = 2020999;
+            byte[] byteHead;
+            byte[] byteData;
+
+
+            if (ltData.ltSet.Count > 0)
+            {
+                start = ((LottoryItem)ltData.ltSet[ltData.ltSet.Count - 1]).id;
+                start += 1;
+            }
+RETRY:
+            try
+            {
+                TcpClient tclient = new TcpClient();
+                tclient.Connect("192.168.1.15", 5000);
+                NetworkStream stream = tclient.GetStream();
+
+                ReqHisData req = new ReqHisData();
+                req.head.key = 0;
+                req.head.magic = IPAddress.HostToNetworkOrder((int)LtNet.LT_MAGIC);
+                req.head.job = IPAddress.HostToNetworkOrder((int)JobId.JOB_HISTORY);
+                req.head.len = IPAddress.HostToNetworkOrder(Marshal.SizeOf(req));
+                req.req.start = IPAddress.HostToNetworkOrder(start);
+                req.req.stop = IPAddress.HostToNetworkOrder(stop);
+
+                stream.Write(StructToBytes(req), 0, Marshal.SizeOf(req));
+                stream.Flush();
+
+                //读取头部
+                ResData res = new ResData();
+                int headSize = Marshal.SizeOf(res);
+                byteHead = new byte[headSize];
+                int count = stream.Read(byteHead, 0, headSize);
+                if (count < headSize)
+                {
+                    stream.Close();
+                    tclient.Close();
+                    MessageBox.Show("更新数据失败：\n");
+                    return;
+                }
+
+                res = (ResData)ltData.BytesToStruct(byteHead, res.GetType());
+                res.head.key = IPAddress.NetworkToHostOrder(res.head.key);
+                res.head.magic = IPAddress.NetworkToHostOrder(res.head.magic);
+                res.head.job = IPAddress.NetworkToHostOrder(res.head.job);
+                res.head.len = IPAddress.NetworkToHostOrder(res.head.len);
+                res.result = IPAddress.NetworkToHostOrder(res.result);
+                if (res.head.magic != LtNet.LT_MAGIC || res.head.job != (int)JobId.JOB_HISTORY || res.head.len < headSize)
+                {
+                    stream.Close();
+                    tclient.Close();
+                    MessageBox.Show("更新数据失败：错误的响应\n");
+                    return;
+                }
+
+                if (res.result != 0)
+                {
+                    stream.Close();
+                    tclient.Close();
+                    MessageBox.Show("更新数据失败：服务器错误\n");
+                    return;
+                }
+
+                if (res.head.len == headSize)
+                {
+                    stream.Close();
+                    tclient.Close();
+                    return;
+                }
+
+                int dataLen = res.head.len - headSize;
+                byteData = new byte[dataLen];
+                int recvLen = 0;
+
+                while (recvLen < dataLen)
+                {
+                    int recvCount = stream.Read(byteData, recvLen, dataLen - recvLen);
+                    if (recvCount <= 0)
+                    {
+                        stream.Close();
+                        tclient.Close();
+                        MessageBox.Show("更新数据失败：\n");
+                        return;
+                    }
+                    recvLen += recvCount;
+                }
+
+                
+            }
+            catch (System.Exception ex)
+            {
+                tryTimes--;
+                if (tryTimes > 0)
+                {
+                    goto RETRY;
+                }
+
+                //MessageBox.Show("更新数据失败：" + ex.Message);
+                return;
+            }
+
+            StreamWriter objWriter = new StreamWriter(DEFINE.HISTORY_FILE, true);   
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string strLt = encoding.GetString(byteData);
+            int itemAdd = 0;
+
+            //objWriter.Write(strLt);
+
+            string[] items = strLt.Split('\n');
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].Length < 10)
+                {
+                    continue;
+                }
+
+                LottoryItem item = new LottoryItem();
+                if (string_to_item(items[i], item) == 0)
+                {
+                    ltData.ltSet.Add(item);
+                    //if (i < (items.Length - 1))
+                    {
+                        items[i] += "\n";
+                    }                    
+                    objWriter.Write(items[i]);
+                    itemAdd++;
+                }
+            }
+            objWriter.Close();
+
+            if (itemAdd > 0)
+            {
+                Thread.Sleep(1000);
+                ltData.form.otherUpdateDatas();
+            }
         }
 
         public static void update_history(object ltd)
